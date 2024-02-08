25ea5bbf695d9f6d2f59cccc23ff78cd87516d69
Kevin Mueller <kevin.mueller@tuta.com>
2024-02-08 12:53:05 +0100

    added no tasks card

--- js/boardRender.js
+++ js/boardRender.js
@@ -2,6 +2,7 @@ function renderCard(){
 
     for (let i = 0; i < 4; i++) {
         const id = document.getElementById(`${i}`);
+        id.innerHTML = '';
         
         if (i == 0) {
             for (let j = 0; j <= 1; j++) {
