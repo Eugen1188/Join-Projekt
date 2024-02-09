5ec0b00a0de73b7cb78c108a2bfc6b389f5d152b
Kevin Müller <kevin.mueller@tuta.com>
2024-02-09 16:50:59 +0100

    implemented complete DnD logic with updates

--- js/board.js
+++ js/board.js
@@ -1,4 +1,4 @@
-let
+let currentDraggedElement;
 
 function testfunction() {
     console.log(allTasks[0][0]);
@@ -14,7 +14,31 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
+function startDragging(id) {
+    currentDraggedElement = id;
+}
 
-function moveTo(id) {
-    allTasks
+async function moveTo(id) {
+    allTasks[currentDraggedElement][0].status.inProgress = false;
+    allTasks[currentDraggedElement][0].status.awaitFeedback = false;
+    allTasks[currentDraggedElement][0].status.done = false;
+
+
+    if (id == 'in-progress') {
+        allTasks[currentDraggedElement][0].status.inProgress = true;
+        await setItem("test_board", allTasks);
+        initBoard();
+    } else if (id == 'await-feedback') {
+        allTasks[currentDraggedElement][0].status.awaitFeedback = true;
+        await setItem("test_board", allTasks);
+        initBoard();
+    } else if (id == 'done') {
+        allTasks[currentDraggedElement][0].status.done = true;
+        await setItem("test_board", allTasks);
+        initBoard();
+    }else if (id == 'todo') {
+        await setItem("test_board", allTasks);
+        initBoard();
+    }
+
 }
