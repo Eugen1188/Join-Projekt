f45355c53c19a1733f78ebeecad011a67ba45a20
Illutas <107239220+Illutas@users.noreply.github.com>
2024-02-10 19:52:40 +0100

    contact singel view added

--- js/contacts.js
+++ js/contacts.js
@@ -1,16 +1,73 @@
 let contacts = [
   {
+    id: 1,
     name: "werner",
     lastname: "müller",
     email: "werner@irgendwas.com",
     phone: "0461669548",
   },
   {
+    id: 2,
     name: "Peter",
     lastname: "hansen",
     email: "perter@gmx.de",
     phone: "016215415742",
+  }, {
+    id: 3,
+    name: "Sabine",
+    lastname: "Schmidt",
+    email: "sabine@example.com",
+    phone: "03012345678",
   },
+  {
+    id: 4,
+    name: "Michael",
+    lastname: "Meier",
+    email: "michael@example.com",
+    phone: "017612345678",
+  },
+  {
+    id: 5,
+    name: "Lisa",
+    lastname: "Müller",
+    email: "lisa@example.com",
+    phone: "049157865432",
+  },
+  {
+    id: 6,
+    name: "Thomas",
+    lastname: "Schulz",
+    email: "thomas@example.com",
+    phone: "071123456789",
+  },
+  {
+    id: 7,
+    name: "Anna",
+    lastname: "Fischer",
+    email: "anna@example.com",
+    phone: "020112345678",
+  },
+  {
+    id: 8,
+    name: "Stefan",
+    lastname: "Wagner",
+    email: "stefan@example.com",
+    phone: "091123456789",
+  },
+  {
+    id: 9,
+    name: "Julia",
+    lastname: "Klein",
+    email: "julia@example.com",
+    phone: "022156789012",
+  },
+  {
+    id: 10,
+    name: "Andreas",
+    lastname: "Schneider",
+    email: "andreas@example.com",
+    phone: "015112345678",
+  }
 ];
 
 
@@ -18,7 +75,10 @@ async function editContact(index) {
 
 }
 
-
 async function deleteContact(index) {
-  
+
+}
+
+function renderContacts() {
+
 }
