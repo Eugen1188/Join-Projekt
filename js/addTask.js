b8e2093f4f797e120c85ec9b368cfed2f3eba7d2
Forstmeister <c.foerster@tutanota.com>
2024-02-15 19:51:17 +0100

    added style and functions to subtasks in addTask.js

--- js/addTask.js
+++ js/addTask.js
@@ -64,16 +64,69 @@ function showSubtasks() {
   showSubtasks.innerHTML = "";
   subtasks.forEach((subtask, index) => {
     showSubtasks.innerHTML += `
-      <li onmouseover="mouseIn()" onmouseout="mouseOut()" id="subtask_${index}">${subtask} <a href="#" onclick="deleteSubtask(${index})">X</a> <a href="#" onclick="editSubtask(${index})">edit</a></li>      `;
+    <div >
+    <li
+      onmouseover="mouseIn(${index})"
+      onmouseout="mouseOut(${index})"
+      id="subtask_${index}"
+      class="subtask-container"
+    >
+      ${subtask} 
+      <div id="subtask-icon-section_${index}" class="d-none">
+      </svg>
+      <svg width="19" height="19" 
+      onclick="editSubtask(${index})"
+          class="pointer" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M2.14453 17H3.54453L12.1695 8.375L10.7695 6.975L2.14453 15.6V17ZM16.4445 6.925L12.1945 2.725L13.5945 1.325C13.9779 0.941667 14.4487 0.75 15.007 0.75C15.5654 0.75 16.0362 0.941667 16.4195 1.325L17.8195 2.725C18.2029 3.10833 18.4029 3.57083 18.4195 4.1125C18.4362 4.65417 18.2529 5.11667 17.8695 5.5L16.4445 6.925ZM14.9945 8.4L4.39453 19H0.144531V14.75L10.7445 4.15L14.9945 8.4Z" fill="#2A3647"/>
+      </svg>
+        <svg
+          onclick="deleteSubtask(${index})"
+          class="pointer"
+          width="25"
+          height="24"
+          viewBox="0 0 25 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <mask
+            id="mask0_75601_14777"
+            style="mask-type: alpha"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="25"
+            height="24"
+          >
+            <rect x="0.144531" width="24" height="24" fill="#D9D9D9" />
+          </mask>
+          <g mask="url(#mask0_75601_14777)">
+            <path
+              d="M7.14453 21C6.59453 21 6.1237 20.8042 5.73203 20.4125C5.34036 20.0208 5.14453 19.55 5.14453 19V6C4.8612 6 4.6237 5.90417 4.43203 5.7125C4.24036 5.52083 4.14453 5.28333 4.14453 5C4.14453 4.71667 4.24036 4.47917 4.43203 4.2875C4.6237 4.09583 4.8612 4 5.14453 4H9.14453C9.14453 3.71667 9.24036 3.47917 9.43203 3.2875C9.6237 3.09583 9.8612 3 10.1445 3H14.1445C14.4279 3 14.6654 3.09583 14.857 3.2875C15.0487 3.47917 15.1445 3.71667 15.1445 4H19.1445C19.4279 4 19.6654 4.09583 19.857 4.2875C20.0487 4.47917 20.1445 4.71667 20.1445 5C20.1445 5.28333 20.0487 5.52083 19.857 5.7125C19.6654 5.90417 19.4279 6 19.1445 6V19C19.1445 19.55 18.9487 20.0208 18.557 20.4125C18.1654 20.8042 17.6945 21 17.1445 21H7.14453ZM7.14453 6V19H17.1445V6H7.14453ZM9.14453 16C9.14453 16.2833 9.24036 16.5208 9.43203 16.7125C9.6237 16.9042 9.8612 17 10.1445 17C10.4279 17 10.6654 16.9042 10.857 16.7125C11.0487 16.5208 11.1445 16.2833 11.1445 16V9C11.1445 8.71667 11.0487 8.47917 10.857 8.2875C10.6654 8.09583 10.4279 8 10.1445 8C9.8612 8 9.6237 8.09583 9.43203 8.2875C9.24036 8.47917 9.14453 8.71667 9.14453 9V16ZM13.1445 16C13.1445 16.2833 13.2404 16.5208 13.432 16.7125C13.6237 16.9042 13.8612 17 14.1445 17C14.4279 17 14.6654 16.9042 14.857 16.7125C15.0487 16.5208 15.1445 16.2833 15.1445 16V9C15.1445 8.71667 15.0487 8.47917 14.857 8.2875C14.6654 8.09583 14.4279 8 14.1445 8C13.8612 8 13.6237 8.09583 13.432 8.2875C13.2404 8.47917 13.1445 8.71667 13.1445 9V16Z"
+              fill="#2A3647"
+            />
+          </g>
+       
+        
+
+      </div>
+    </li>
+  </div>
+      `;
   });
 }
 
-function mouseIn() {
-  console.log("rein");
+function mouseIn(index) {
+  let selectedSubtask = document.getElementById(`subtask_${index}`);
+  let iconSection =document.getElementById(`subtask-icon-section_${index}`);
+  selectedSubtask.classList.add("subtask-selected");
+  iconSection.classList.remove("d-none");
 }
 
-function mouseOut() {
-  console.log("raus");
+function mouseOut(index) {
+  let selectedSubtask = document.getElementById(`subtask_${index}`);
+  let iconSection =document.getElementById(`subtask-icon-section_${index}`);
+  selectedSubtask.classList.remove("subtask-selected");
+  iconSection.classList.add("d-none");
 }
 
 function deleteSubtask(index) {
