6fa0aafd3aae71b951b386125f0482f0c8441aa2
Kevin Mueller <kevin.mueller@tuta.com>
2024-02-27 10:21:48 +0100

    added drag n drop menu for mobile version

---
+++ js/boardResponsive.js
@@ -0,0 +1,16 @@
+function dragMenu(id){
+    let task = allTasks[id][0].status;
+    currentDraggedElement = id;
+
+    document.getElementById(`drop-menu${id}`).classList.toggle('d-none');
+
+    if (task.inProgress === true) {
+        document.getElementById(`drop-inprogress${id}`).classList.toggle('drop-menu-disabled');
+    }else if(task.awaitFeedback === true){
+        document.getElementById(`drop-awaitfeedback${id}`).classList.toggle('drop-menu-disabled');
+    }else if(task.done === true){
+        document.getElementById(`drop-done${id}`).classList.toggle('drop-menu-disabled');
+    }else{
+        document.getElementById(`drop-todo${id}`).classList.toggle('drop-menu-disabled');
+    }
+}
