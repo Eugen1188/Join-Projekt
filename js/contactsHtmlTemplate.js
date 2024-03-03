eed41948fd519c0f120bf8ab9f22700fbf4f3ca6
Illutas <107239220+Illutas@users.noreply.github.com>
2024-03-03 10:10:44 +0100

    word on mobile responsive

--- js/contactsHtmlTemplate.js
+++ js/contactsHtmlTemplate.js
@@ -118,7 +118,7 @@ function contactsCardHTML({ cardName, secondText, functionName, secontFunction, 
 
             </div>
             <div class="edit-card-form">
-                <div class="mobileCirclePosition" id="circle-color">
+                <div class="mobileCirclePosition small-screen-display-none" id="circle-color">
                 </div>
                 <div class="edit-card-form-input">
                     <form onsubmit="${functionName}; return false" id="contacts-form">
@@ -127,11 +127,11 @@ function contactsCardHTML({ cardName, secondText, functionName, secontFunction, 
                         <input class="edit-card-from-input" type="tel" placeholder="Phone" id="phone" maxlength="11">
                         <div class="edit-card-btn-wrapper">
                             <div id="delete-btn">
-                                <button class="edit-card-btn white-btn pointer" id="delete" onclick="${secontFunction}">${deleteOrClosebtn}</button>
+                                <button class="edit-card-btn white-btn pointer mobile-btn-font-size" id="delete" onclick="${secontFunction}">${deleteOrClosebtn}</button>
                             </div>
                             <button class="edit-card-btn main-btn-color font-color pointer" type="submit" onclick="${functionName}">
-                                <span>${saveOrCreateContact}</span>
-                                <img src="./assets/img/icons/check.png" alt="">
+                                <span class="mobile-btn-font-size">${saveOrCreateContact}</span>
+                                <img class="small-screen-display-none" src="./assets/img/icons/check.png" alt="">
                             </button>
                         </div>
                     </form>
