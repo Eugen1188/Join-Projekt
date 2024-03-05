c6bf69e730c35b5a7c10c2b438c849b3e6b6dd52
Illutas <107239220+Illutas@users.noreply.github.com>
2024-03-05 18:38:01 +0100

    initial fix

--- js/contacts.js
+++ js/contacts.js
@@ -114,7 +114,7 @@ async function saveNewUserData() {
     lastname: lastname[lastname.length - 1],
     email: email,
     password: password,
-    initials: firstname[0].charAt(0).toUpperCase() + lastname[lastname.length - 1].charAt(0),
+    initials: helper.length === 0 ? helper[0].charAt(0).toUpperCase() : helper[0].charAt(0).toUpperCase() + helper[1].charAt(0).toUpperCase(), ,
     circleColor: getRandomColor(),
     phone: "No data stored",
   });
